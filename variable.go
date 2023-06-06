de2a00f948b1a4fcecd6331d278925b99e9af6a2
Yogish M <yogishmohan2000@gmail.com>
2023-06-06 12:57:06 +0530

    float var added

--- variable.go
+++ variable.go
@@ -4,5 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello Yogish!")
-	var a int //declared a int variable
+	var a int   //declared a int variable
+	var b float //declared a float varialble
 }
