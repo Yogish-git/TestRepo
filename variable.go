b3221b8b24889b366eb3ebc321636ba0e57fb918
Yogish M <yogishmohan2000@gmail.com>
2023-06-06 13:10:24 +0530

    added array

--- variable.go
+++ variable.go
@@ -4,6 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello Yogish!")
-	var a int   //declared a int variable
-	var b float //declared a float varialble
+	var a int = 7 //declared a int variable
+
+	i := 113 //shortcut for declaration
+	fmt.Println(a, i)
+
+	//arrays in go
+	array := [4]string{"apple", "orange", "grapes", "pear"}
+	fmt.Println(array)
+
 }
