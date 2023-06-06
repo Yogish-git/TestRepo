53bdd11f34012d201f842c47350609fe075ad8b9
Yogish M <yogishmohan2000@gmail.com>
2023-06-06 13:20:24 +0530

    sturcts added

--- variable.go
+++ variable.go
@@ -13,4 +13,24 @@ func main() {
 	array := [4]string{"apple", "orange", "grapes", "pear"}
 	fmt.Println(array)
 
+	//structs
+	//declaring struct
+	type Employee struct {
+		firstName string
+		lastName  string
+		age       int
+	}
+
+	//initialising struct instance
+	emp1 := Employee{
+		firstName: "Ram",
+		lastName:  "Kumar",
+		age:       20,
+	}
+
+	emp2 := Employee{"Yogish", "M", 22}
+
+	fmt.Println(emp1)
+	fmt.Println(emp2)
+
 }
